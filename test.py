b9cfe369af98ef1a8842cdc2c5177fd3eb770db7
martinfrnka <martin.frnka@gmail.com>
2026-05-02 00:14:55 +0200

    commit and push - tilepicker implemented and selected tile displayed on the left corner

--- test.py
+++ test.py
@@ -1,31 +1,23 @@
 import pygame
-
+from pygame.locals import *
 pygame.init()
-width = 800
-height = 600
-screen = pygame.display.set_mode((width, height), pygame.RESIZABLE)
-changed = False
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()
-        elif event.type == pygame.VIDEORESIZE:
-            screen = pygame.display.set_mode((event.w, event.h), pygame.RESIZABLE)
-        if event.type ==pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                width -=20
-                changed = True
-            if event.key == pygame.K_RIGHT:
-                width +=20
-                changed = True
-            if event.key == pygame.K_UP:
-                height -=20
-                changed = True
-            if event.key == pygame.K_DOWN:
-                height +=20
-                changed = True
-    if changed:
-        screen = pygame.display.set_mode((width, height), pygame.RESIZABLE)
-        changed = False
-    
+screen = pygame.display.set_mode((640, 480))
+clock = pygame.time.Clock()
+
+def main():
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                return
+            elif event.type == MOUSEWHEEL:
+                print(event)
+                print(event.x, event.y)
+                print(event.flipped)
+                #print(event.which)
+                # can access properties with
+                # proper notation(ex: event.y)
+        clock.tick(60)
+
+# Execute game:
+main()
